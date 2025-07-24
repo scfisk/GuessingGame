d7d49de8851e57935ef8bf8a831fd357eb334a4d
Sara Nuss <35274866+scfisk@users.noreply.github.com>
2025-07-23 22:03:17 -0400

    Added some things

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,57 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import com.sun.tools.javac.Main;
+/**
+ * Program by Sara Fisk
+ * A simple guessing game to familiarize myself with the basics of
+ * java and get used to a new IDE
+ */
+
 import java.util.Scanner;
 
 public class GuessingGame {
 
-    // returns an interger entered by user
+    // get an int from the user
     static int GetInteger() {
         Scanner scn = new Scanner(System.in);
-        return scn.nextInt();
+        //check if it's an integer, if it is...
+        if (scn.hasNextInt()) {
+            // return it
+            return scn.nextInt();
+        } else {
+            System.out.println("Please enter an integer!");
+            // if it's not, call this function again until an integer is entered
+            final int i = GetInteger();
+            return i;
+        }
     }
 
-    // generate and return a random number in given range
+    // random number for user to guess
     static int GenerateRandomNumber(int min, int max) {
-        return (int)(Math.random() * max) + min;
+        return (int) (Math.random() * max) + min;
     }
 
-    // return the user's guess
+    // get a guess from the user
     static int GetGuess() {
         System.out.println("Type your guess: ");
         return GetInteger();
     }
 
-    // check if the guess is correct. Tell user if it's too high or too low
-    static boolean IsCorrect(int target, int guess) {
-        // too high
-        if (guess > target) {
-            System.out.println("Incorrect. Too high!");
+    // check the guess
+    static boolean isCorrect(int target, int guess) {
+        // make sure it's in range
+        if ((guess > 100) || (guess < 1)) {
+            System.out.println("Guess is out of range!");
             return false;
-        }
-        // too low
-        else if (guess < target) {
+        } else if (guess < target) {
             System.out.println("Incorrect. Too low!");
             return false;
-        }
-        // just right
-        else {
-            System.out.println( "Correct!");
+        } else if (guess > target) {
+            System.out.println("Incorrect. Too high!");
+            return false;
+        } else {
+            System.out.println("Correct!");
             return true;
         }
     }
 
-    // part of an attempt to allow to user to choose to get the hint or not.
-   // static String GetHint(int hintNumber) {
-    //    return switch (hintNumber) {
-     //       case 0 -> "Too high!";
-       //     case 1 -> "Too low!";
-     //       default -> "No hint!";
-    //   };
-   // }
+    static String GetHint(int hintNumber) {
+        return switch (hintNumber) {
+            case 0 -> "Too high!";
+            case 1 -> "Too low!";
+            default -> "No hint!";
+        };
 
-    // displays main menu and returns user's selection
-    static int MainMenuOptions(){
+    }
+
+    static int MainMenuOptions() {
         System.out.println("Welcome to Guess the Number! Please select and option:");
         System.out.println("1 - Play Game");
         System.out.println("2 - See Instructions");
@@ -59,46 +69,38 @@ public class GuessingGame {
         return GetInteger();
     }
 
-    // actual game play
+
     static void PlayGame() {
-        // create number to be guessed
-        int target = GenerateRandomNumber(1,100);
-        // index for loop
+        int target = GenerateRandomNumber(1, 100);
         int i = 0;
         boolean isPlaying = true;
-        // loop until number is guessed or out of turns
         while (isPlaying && (i < 8)) {
             int guess = GetGuess();
-            boolean isCorrect = IsCorrect(target,guess);
+            boolean isCorrect = isCorrect(target, guess);
             if (isCorrect)
                 isPlaying = false;
             else
                 i++;
         }
-        if (i == 8){
-            System.out.println("All out of guesses!");
+        if (i == 8) {
+            System.out.println("All out of guesses! It was " + target);
         }
     }
 
-    // display instructions for game
-    static void DisplayInstructions(){
+    static void DisplayInstructions() {
         System.out.println("The computer will generate a random, positive number between 1 and 100. " +
                 "You will then have 8 tries to guess the number correctly. After each guess you'll be given " +
                 "the option to guess again, or receive a hint. The hint will tell you whether your guess was" +
                 "too high or too low. Those hints can really come in handy!");
     }
-    
-    // end the program
-    static void EndGame(){
+
+    static void EndGame() {
         System.out.println("Goodbye!");
         System.exit(0);
     }
 
-    // main function - run the whole thing
     public static void main(String[] args) {
-        // loop signal
         boolean keepGoing = true;
-        //play until user wants to stop
         while (keepGoing) {
             int choice = MainMenuOptions();
             switch (choice) {
@@ -116,4 +118,5 @@ public class GuessingGame {
         }
 
     }
+
 }
